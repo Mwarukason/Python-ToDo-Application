7faa8e351fa2a10d3a90757119fa005f60623d1d
Mwarukason <a.mwaruka93@gmail.com>
2017-06-13 09:26:43 +0800

    create function

--- funct_methoz.py
+++ funct_methoz.py
@@ -14,3 +14,21 @@ def Databaseconnect():
     cursor = conn.cursor()
     cursor.execute(
         "CREATE TABLE IF NOT EXISTS `todolist` (task_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, todovalue TEXT)")
+
+#method to create a task and insrt into database
+def Create():
+    if TODO.get() == "":
+        txt_output.config(text="Sorry the field is empty!", fg="red")
+    else:
+        Databaseconnect()
+        cursor.execute("INSERT INTO `todolist` (todovalue) VALUES(?)", (str(TODO.get()),))
+        tree.delete(*tree.get_children())
+        cursor.execute("SELECT * FROM `todolist` ORDER BY `todovalue` ASC")
+        fetch = cursor.fetchall()
+        for data in fetch:
+            tree.insert('', 'end', values=(data[0]))
+        conn.commit()
+        TODO.set("")
+        cursor.close()
+        conn.close()
+        txt_output.config(text="Created a data!", fg="blue")
